47ca05fb1228b663dd71ae06bac54a79385c9309
anggiprasetyoo <152991609+anggiprasetyoo@users.noreply.github.com>
2024-01-18 12:15:29 +0700

    Delete go crud directory

--- go crud/entities/customer.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type Customer struct {
-	Id          int64
-	NamaLengkap string `validate:"required" label:"Nama Lengkap"`
-	NomorHp     string `validate:"required" label:"Nomor HP"`
-	Merk        string `validate:"required"`
-	Alamat      string `validate:"required"`
-}
